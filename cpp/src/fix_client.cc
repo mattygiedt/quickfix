77da1cf021949d8848df5446d9039e5ec6229ac9
Matthew Giedt <mgiedt@gmail.com>
2021-11-16 22:50:48 -0600

    add signal_handler.h

--- cpp/src/fix_client.cc
+++ cpp/src/fix_client.cc
@@ -4,31 +4,7 @@
 
 #include "client_app.h"
 #include "common/application_traits.h"
-
-std::atomic<bool> running_;
-std::mutex running_mutex_;
-std::condition_variable running_cv_;
-
-auto SignalHandler(int /*unused*/) -> void {
-  running_ = false;
-  running_cv_.notify_all();
-}
-
-auto SetupSignalHandler() -> void {
-  struct sigaction sig_int_handler;
-  sig_int_handler.sa_handler = SignalHandler;
-  sigemptyset(&sig_int_handler.sa_mask);
-  sig_int_handler.sa_flags = 0;
-  sigaction(SIGINT, &sig_int_handler, nullptr);
-}
-
-auto WaitForSignal() -> void {
-  running_ = true;
-  while (running_) {
-    std::unique_lock<decltype(running_mutex_)> lock(running_mutex_);
-    running_cv_.wait(lock);
-  }
-}
+#include "common/signal_handler.h"
 
 template <typename Traits>
 class FixClient : public Traits {
@@ -87,7 +63,7 @@ class FixClient : public Traits {
 auto main(int argc, char** argv) -> int {
   if (argc < 2) {
     std::cout << "usage: " << argv[0] << " FILE." << std::endl;
-    return 0;
+    return 1;
   }
 
   std::string file = argv[1];
@@ -99,5 +75,6 @@ auto main(int argc, char** argv) -> int {
   client.SendOrder();
   WaitForSignal();
   client.Stop();
-  return 1;
+
+  return 0;
 }
--- cpp/src/fix_server.cc
+++ cpp/src/fix_server.cc
@@ -3,32 +3,8 @@
 #include <thread>
 
 #include "common/application_traits.h"
+#include "common/signal_handler.h"
 #include "server_app.h"
-
-std::atomic<bool> running_;
-std::mutex running_mutex_;
-std::condition_variable running_cv_;
-
-auto SignalHandler(int /*unused*/) -> void {
-  running_ = false;
-  running_cv_.notify_all();
-}
-
-auto SetupSignalHandler() -> void {
-  struct sigaction sig_int_handler;
-  sig_int_handler.sa_handler = SignalHandler;
-  sigemptyset(&sig_int_handler.sa_mask);
-  sig_int_handler.sa_flags = 0;
-  sigaction(SIGINT, &sig_int_handler, nullptr);
-}
-
-auto WaitForSignal() -> void {
-  running_ = true;
-  while (running_) {
-    std::unique_lock<decltype(running_mutex_)> lock(running_mutex_);
-    running_cv_.wait(lock);
-  }
-}
 
 template <typename Traits>
 class FixServer {
@@ -81,7 +57,7 @@ class FixServer {
 auto main(int argc, char** argv) -> int {
   if (argc < 2) {
     std::cout << "usage: " << argv[0] << " FILE." << std::endl;
-    return 0;
+    return 1;
   }
 
   std::string file = argv[1];
@@ -92,5 +68,6 @@ auto main(int argc, char** argv) -> int {
   server.Start();
   WaitForSignal();
   server.Stop();
-  return 1;
+
+  return 0;
 }
